1370be5a26f51e33691cae86fc6dd679457bf597
Nick DHUPIA <nick.dhupia@dailymotion.com>
2014-08-19 01:05:48 -0700

    adding test for de

---
+++ de_test.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	de "github.com/dailymotion/pixelle-de/delib"
+	"log"
+	"testing"
+	"time"
+)
+
+func NewData() map[string]interface{} {
+	var dst = make(map[string]interface{})
+	var (
+		timenow = time.Now().Format(time.RFC3339)
+		data    = map[string]interface{}{
+			"_id":                 "1",
+			"ad":                  "1",
+			"duration":            123,
+			"campaign":            "1",
+			"_updated":            timenow,
+			"_created":            timenow,
+			"categories":          []string{"cat1", "cat2"},
+			"devices":             []string{"dev1", "dev2"},
+			"title":               "title",
+			"description":         "description",
+			"channel":             "channel",
+			"channel_url":         "http://channel-url",
+			"formats":             []string{"format1", "format2"},
+			"thumbnail_url":       "http://thumbnail-url",
+			"video_url":           "http://video-url",
+			"tactic":              "1",
+			"status":              "active",
+			"account":             "1",
+			"goal_views":          1999999,
+			"goal_period":         "total",
+			"cpc":                 1.12,
+			"locations":           []string{"us", "fr"},
+			"languages":           []string{"en", "fr"},
+			"excluded_categories": []string{"ec1", "ec2"},
+			"excluded_locations":  []string{"el1", "el2"},
+		}
+	)
+	for k, v := range data {
+		dst[k] = v
+	}
+	return dst
+}
+func TestInsertAndQuery(t *testing.T) {
+	d := NewData()
+	defer clean("1", t)
+	in := loadData(d, t)
+	sq := de.SearchQuery{
+		Device:     "dev1",
+		Locations:  []string{"fr"},
+		Languages:  []string{"en"},
+		AdFormat:   "format1",
+		Categories: []string{"cat1"},
+	}
+	//wait for data to be loaded
+	time.Sleep(2 * time.Second)
+	adunitOut, err := de.QueryUniqAdFromES(20, 20, sq)
+	if err != nil {
+		t.Fail()
+	}
+	Assert(len(adunitOut) == 1, t, "testing", nil)
+	deepEqual(in, &adunitOut[0], t)
+}
+func TestAllMatch(t *testing.T) {
+	d := NewData()
+	d["categories"] = nil
+	d["locations"] = nil
+	d["languages"] = nil
+	d["formats"] = nil
+	d["devices"] = nil
+
+	defer clean("1", t)
+	loadData(d, t)
+	sq := de.SearchQuery{}
+
+	//wait for data to be loaded
+	time.Sleep(2 * time.Second)
+	adunitOut, err := de.QueryUniqAdFromES(2, 2, sq)
+	if err != nil {
+		t.Fail()
+	}
+	Assert(len(adunitOut) == 1, t, "testing", nil)
+}
+
+func TestDuplicateCampaigns(t *testing.T) {
+	d1 := NewData()
+	d2 := NewData()
+	d2["_id"] = "2"
+	d2["ad"] = "2"
+	defer clean("1", t)
+	defer clean("2", t)
+	in1 := loadData(d1, t)
+	loadData(d2, t)
+	sq := de.SearchQuery{
+		Device:     "dev1",
+		Locations:  []string{"fr"},
+		Languages:  []string{"en"},
+		AdFormat:   "format1",
+		Categories: []string{"cat1"},
+	}
+	//wait for data to be loaded
+	time.Sleep(2 * time.Second)
+	unitsOut, err := de.QueryUniqAdFromES(2, 2, sq)
+	if err != nil {
+		t.Fail()
+	}
+	Assert(len(unitsOut) == 1, t, "testing", nil)
+	deepEqual(in1, &unitsOut[0], t)
+}
+
+func deepEqual(a *de.Unit, b *de.Unit, t *testing.T) {
+	//Assert(a.GoalViews == b.GoalViews, t, "testing", nil)
+	//Assert(a.Id == b.Id, t, "testing", nil)
+	//Assert(a.Ad == b.Ad, t, "testing", nil)
+	Assert(a.TacticId == b.TacticId, t, "testing", nil)
+	Assert(a.CampaignId == b.CampaignId, t, "testing", nil)
+	Assert(a.Channel == b.Channel, t, "testing", nil)
+	Assert(a.Description == b.Description, t, "testing", nil)
+	Assert(a.Account == b.Account, t, "testing", nil)
+	Assert(a.Cpc == b.Cpc, t, "testing", nil)
+	Assert(a.ChannelUrl == b.ChannelUrl, t, "testing", nil)
+	Assert(a.ThumbnailUrl == a.ThumbnailUrl, t, "testing", nil)
+	Assert(a.VideoUrl == b.VideoUrl, t, "testing", nil)
+	Assert(a.Account == b.Account, t, "testing", nil)
+	//Assert(a.GoalPeriod == b.GoalPeriod, t, "testing", nil)
+	//Assert(a.Status == b.Status, t, "testing", nil)
+	Assert(a.Title == b.Title, t, "testing", nil)
+	//Assert(a.Created.Equal(*b.Created), t, "testing", nil)
+	//Assert(a.Updated.Equal(*b.Updated), t, "testing", nil)
+	Assert(a.Duration == b.Duration, t, "testing", nil)
+	//Assert(len(a.Locations) == len(b.Locations), t, "testing", nil)
+	//Assert(len(a.Languages) == len(b.Languages), t, "testing", nil)
+	//Assert(len(a.AdFormats) == len(b.AdFormats), t, "testing", nil)
+	//Assert(len(a.Devices) == len(b.Devices), t, "testing", nil)
+	//Assert(len(a.Categories) == len(b.Categories), t, "testing", nil)
+	//Assert(len(a.ExcludedCategories) == len(b.ExcludedCategories), t, "testing", nil)
+	//Assert(len(a.ExcludedLocations) == len(b.ExcludedLocations), t, "testing", nil)
+
+}
+
+// dumb simple assert for testing, printing
+//    Assert(len(items) == 9, t, "Should be 9 but was %d", len(items))
+func Assert(is bool, t *testing.T, format string, args ...interface{}) {
+	if is == false {
+		log.Printf(format, args...)
+		t.Fail()
+	}
+}
+
+func printAdUnit(ad *de.Unit) {
+	b, err := json.MarshalIndent(ad, "", "    ")
+	if err == nil {
+		fmt.Println(string(b))
+	}
+}
+
+func loadData(v interface{}, t *testing.T) *de.Unit {
+	var (
+		in de.Unit
+	)
+	br1, _ := json.Marshal(v)
+	err1 := json.Unmarshal(br1, &in)
+	if err1 != nil {
+		t.Fail()
+	}
+	err2 := de.UpdateAd(&in)
+	if err2 != nil {
+		t.Fail()
+	}
+	return &in
+
+}
+func init() {
+	fmt.Println("Creating Index")
+	de.CreateIndex()
+	time.Sleep(3 * time.Second)
+}
+
+func clean(id string, t *testing.T) {
+	err := de.DeleteAdUnitById(id)
+	if err != nil {
+		t.Fail()
+	}
+}
