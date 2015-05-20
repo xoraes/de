12cd99b6a91774b4a6bd677d11e21cb479d91c98
xoraes <xoraes@gmail.com>
2015-05-19 17:47:15 -0700

    added lombok data to DeException

--- src/main/java/com/dailymotion/pixelle/deserver/processor/DeException.java
+++ src/main/java/com/dailymotion/pixelle/deserver/processor/DeException.java
@@ -1,11 +1,15 @@
 package com.dailymotion.pixelle.deserver.processor;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Created by n.dhupia on 11/4/14.
  */
+@Data
+@EqualsAndHashCode(callSuper = false)
 public class DeException extends Exception {
     private static final Logger logger = LoggerFactory.getLogger(DeException.class);
 
