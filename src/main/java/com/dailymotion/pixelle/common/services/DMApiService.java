19ad985b88ec2110155ed1ecbb6d1447058febcd
xoraes <xoraes@gmail.com>
2015-09-22 17:48:16 -0700

    get 50 videos for my channel

--- src/main/java/com/dailymotion/pixelle/common/services/DMApiService.java
+++ src/main/java/com/dailymotion/pixelle/common/services/DMApiService.java
@@ -12,7 +12,7 @@ import feign.RequestLine;
 public interface DMApiService {
     @RequestLine("GET /videos?owners={channels}&fields=id,3d,ads,allow_embed,channel,owner.screenname,created_time," +
             "updated_time,description,duration,explicit,geoblocking,language,mediablocking,mode,owner.id,owner" +
-            ".username,published,status,tags,thumbnail_url,title,&sort={order}&limit=250")
+            ".username,published,status,tags,thumbnail_url,title,&sort={order}&limit=50")
     ChannelVideos getVideos(@Param("channels") String channelIds, @Param("order") String sortOrder) throws DeException;
 
 }
